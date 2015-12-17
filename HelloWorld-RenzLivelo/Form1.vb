dccb6511b7d3a50267e9e2f3b1a020a774c62839
Renz Angelo D. Livelo <dynamic.radlivelo@gmail.com>
2015-12-18 00:56:17 +0800

    VB - Hello World
    VB Hello World BSCpE 5-2 Elective

---
+++ HelloWorld-RenzLivelo/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.Visible = False
+        Label1.Text = "Hello World!"
+    End Sub
+End Class
